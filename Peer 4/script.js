d13ea947582c05001b3b89b480c0e3e2eb62e364
SmaranikaBiswas <sbiswas1506@gmail.com>
2021-09-27 20:45:45 +0530

    Update

--- Peer 4/script.js
+++ Peer 4/script.js
@@ -1,12 +1,16 @@
 var names=new Array();
-names[0]="Yadav";
-names[1]="Jonny";
-names[2]="jay";
-names[3]="Freddy";
-names[4]="Rose";
-names[5]="Benny";
-names[6]="Jen";
-names[7]="jim";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
+
+
 
 for (var i = 0; i < names.length; i++)
 {
